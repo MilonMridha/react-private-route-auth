6314c945963a5c92bf53b91bcfbf466678e6c4c2
MilonMridha <milonmridha570@gmail.com>
2022-04-09 08:28:24 -0700

    added-route

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,6 +1,7 @@
 import { getAuth } from 'firebase/auth';
 import React from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useLocation, useNavigate } from 'react-router-dom';
 import app from '../../firebase.init';
 
 
@@ -9,12 +10,21 @@ const auth = getAuth(app)
 const Login = () => {
 
     const [signInWithGoogle, user] = useSignInWithGoogle(auth);
-    
+    const location = useLocation();
+    const navigate = useNavigate();
+    const from = location.state?.from?.pathname || '/';
+
+    const handleGoogleSignIn = () => {
+        signInWithGoogle()
+        .then(() =>{
+            navigate(from,  { replace: true })
+        })
+    }
     return (
         <div>
-            
+
             <h4>Welcome to login page</h4>
-            <button onClick={() => signInWithGoogle()}>SignIn with Google</button>
+            <button onClick={handleGoogleSignIn}>SignIn with Google</button>
             <br />
             <br />
             <input type="text" placeholder='email' />
